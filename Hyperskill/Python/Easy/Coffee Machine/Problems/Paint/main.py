5cddefc4d7d0d01e2cdbb8f294332b20ae35eadc
Daniel Burbridge <36094258+DanielJBurbridge@users.noreply.github.com>
2021-02-05 12:05:43 +0800

    Accidentally uploaded additonal files, removing

--- Hyperskill/Python/Easy/Coffee Machine/Problems/Paint/main.py
+++
@@ -1,8 +0,0 @@
-class House:
-    def __init__(self, floors):
-        self.floors = floors
-
-    # create the method here
-
-    def paint(self, color):
-        self.color = color
--- Hyperskill/Python/Easy/Coffee Machine/Problems/The Creator/main.py
+++
@@ -1,4 +0,0 @@
-# create an Elf here
-class Elf:
-
-    height, weapon, emotional_maturity = 1.8, "longbow", 125
