ebeaf4cbc9b7b5557660036a0959c48f06e3770c
Daniel Burbridge <36094258+DanielJBurbridge@users.noreply.github.com>
2021-02-12 16:05:35 +0800

    Started and Completed Zookeeper

---
+++ Hyperskill/Python/Easy/Zookeeper/Zookeeper/2.0/zookeeper.py
@@ -0,0 +1,24 @@
+camel = r"""
+Switching on the camera in the camel habitat...
+ ___.-''''-.
+/___  @    |
+',,,,.     |         _.'''''''._
+     '     |        /           \
+     |     \    _.-'             \
+     |      '.-'                  '-.
+     |                               ',
+     |                                '',
+      ',,-,                           ':;
+           ',,| ;,,                 ,' ;;
+              ! ; !'',,,',',,,,'!  ;   ;:
+             : ;  ! !       ! ! ;  ;   :;
+             ; ;   ! !      ! !  ; ;   ;,
+            ; ;    ! !     ! !   ; ;
+            ; ;    ! !    ! !     ; ;
+           ;,,      !,!   !,!     ;,;
+           /_I      L_I   L_I     /_I
+Look at that! Our little camel is sunbathing!"""
+
+print("Switching on the camera in the camel habitat...")
+print(camel)
+print("Look at that! our little camel is sunbathing!")
